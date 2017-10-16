40945d10fe7de2560a562078f08f30a92b514b8b
Sergio Ocon <sergio.ocon@redhat.com>
2017-10-16 13:59:22 +0200

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,18 +7,11 @@ class User < ApplicationRecord
          :confirmable, :lockable, :timeoutable
 
   # Validates name
-  validates :first_name, presence: true, length: { within: 2..80 }
-  validates :last_name,  presence: true, length: { within: 2..80 }
-  # Validates phone number
-  VALID_TELEPHONE_REGEX = /\A(\+\d\d|00\d\d)?\d{9}\z/
-  validates :phone, presence: true, length: { within: 9..13}
-  validates :phone, format: { with: VALID_TELEPHONE_REGEX}
+  validates :full_name,  presence: true, length: { within: 2..80 }
+  validates :sort_name,  presence: true, length: { within: 2..80 }
+
   # Has Offspring
   has_many :offsprings, dependent: :destroy
-
-  def full_name
-    "#{first_name} #{last_name}"
-  end
 
   def self.to_csv
     attributes = %w[id first_name last_name phone email]
